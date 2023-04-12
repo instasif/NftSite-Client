0470ccb04f77e21e6b4401a9e435b560ce060207
Asif Hossain <asifh9277@gmail.com>
2023-04-12 16:59:03 +0600

    deleted some extra files

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-import Hero from "../../Componants/Hero/Hero";
 import TodaysPic from "../../Componants/TodaysPic/TodaysPic";
 import LiveActions from "../../Componants/LiveActions/LiveActions";
 import CreateAndSellYourNFTs from "../../Componants/Create and Sell Your NFT/CreateAndSellYourNFTs";
 import PopularCollections from "../../Componants/Popular Collection/PopularCollections";
 import TopSellers from "../../Componants/Top Seller/TopSellers";
-import TestHero from "../../Componants/Hero/TestHero";
+import Hero from '../../Componants/Hero/Hero'
 
 const Home = () => {
   return (
     <div>
-      {/* <Hero /> */}
-      <TestHero />
+      <Hero />
       <LiveActions />
       <TopSellers />
       <TodaysPic />
